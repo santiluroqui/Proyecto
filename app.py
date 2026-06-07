7247287417cb88422ccbb26128f6582941400c79
santiluroqui <santyluroqui@gmail.com>
2026-06-07 19:57:55 -0400

    main
    sdeafda

--- app.py
+++ app.py
@@ -299,5 +299,12 @@ def get_campanas():
         print(f"❌ [Campañas] ERROR: {e}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
